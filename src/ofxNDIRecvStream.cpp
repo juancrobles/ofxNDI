ccbdea16cd16242ed5221ffa264f57589fb86e44
Juan C Robles <juancrobles@hotmail.com>
2023-11-26 06:33:51 -0600

    Fixed ndi receiver & frame

--- src/ofxNDIRecvStream.cpp
+++ src/ofxNDIRecvStream.cpp
@@ -1,164 +1,164 @@
-//#include "ofxNDIRecvStream.h"
-//#include <Processing.NDI.AVSync.h>
-//
-//#pragma mark Video Stream
-//
-//template<>
-//int ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getQueue() const
-//{
-//	NDIlib_recv_queue_t frames;
-//	NDIlib_recv_get_queue(instance_, &frames);
-//	return frames.video_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getNumReceivedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, &frames, nullptr);
-//	return frames.video_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getNumDroppedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, nullptr, &frames);
-//	return frames.video_frames;
-//}
+#include "ofxNDIRecvStream.h"
+#include <Processing.NDI.FrameSync.h>
+
+#pragma mark Video Stream
+
+template<>
+int ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getQueue() const
+{
+	NDIlib_recv_queue_t frames;
+	NDIlib_recv_get_queue(instance_, &frames);
+	return frames.video_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getNumReceivedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, &frames, nullptr);
+	return frames.video_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::getNumDroppedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, nullptr, &frames);
+	return frames.video_frames;
+}
 //template<> template<>
 //inline void ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::setAllocator(void* p_opaque, NDIlib_video_alloc_t p_allocator, NDIlib_video_free_t p_deallocator) {
 //	NDIlib_recv_set_video_allocator(instance_, p_opaque, p_allocator, p_deallocator);
 //}
-//
-//template<>
-//void ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::freeFrame(ofxNDI::VideoFrame &frame) {
-//	NDIlib_recv_free_video_v2(instance_, &frame);
-//}
-//template<>
-//bool ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::captureFrame(ofxNDI::VideoFrame &frame) {
-//	return NDIlib_recv_capture_v2(instance_, &frame, nullptr, nullptr, timeout_ms_) == NDIlib_frame_type_video;
-//}
-//bool ofxNDIRecvVideoFrameSync::captureFrame(ofxNDI::VideoFrame &frame) {
-//	NDIlib_framesync_capture_video(sync_, &frame, field_type_);
-//	return frame.p_data != nullptr;
-//}
-//void ofxNDIRecvVideoFrameSync::freeFrame(ofxNDI::VideoFrame &frame) {
-//	NDIlib_framesync_free_video(sync_, &frame);
-//}
-//
-//#pragma mark Audio Stream
-//
-//template<>
-//int ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getQueue() const
-//{
-//	NDIlib_recv_queue_t frames;
-//	NDIlib_recv_get_queue(instance_, &frames);
-//	return frames.audio_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getNumReceivedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, &frames, nullptr);
-//	return frames.audio_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getNumDroppedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, nullptr, &frames);
-//	return frames.audio_frames;
-//}
+
+template<>
+void ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::freeFrame(ofxNDI::VideoFrame &frame) {
+	NDIlib_recv_free_video_v2(instance_, &frame);
+}
+template<>
+bool ofxNDI::Recv::Stream<ofxNDI::VideoFrame>::captureFrame(ofxNDI::VideoFrame &frame) {
+	return NDIlib_recv_capture_v2(instance_, &frame, nullptr, nullptr, timeout_ms_) == NDIlib_frame_type_video;
+}
+bool ofxNDIRecvVideoFrameSync::captureFrame(ofxNDI::VideoFrame &frame) {
+	NDIlib_framesync_capture_video(sync_, &frame, field_type_);
+	return frame.p_data != nullptr;
+}
+void ofxNDIRecvVideoFrameSync::freeFrame(ofxNDI::VideoFrame &frame) {
+	NDIlib_framesync_free_video(sync_, &frame);
+}
+
+#pragma mark Audio Stream
+
+template<>
+int ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getQueue() const
+{
+	NDIlib_recv_queue_t frames;
+	NDIlib_recv_get_queue(instance_, &frames);
+	return frames.audio_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getNumReceivedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, &frames, nullptr);
+	return frames.audio_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::getNumDroppedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, nullptr, &frames);
+	return frames.audio_frames;
+}
 //template<> template<>
 //inline void ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::setAllocator(void* p_opaque, NDIlib_audio_alloc_t p_allocator, NDIlib_audio_free_t p_deallocator) {
 //	NDIlib_recv_set_audio_allocator(instance_, p_opaque, p_allocator, p_deallocator);
 //}
-//
-//template<>
-//void ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::freeFrame(ofxNDI::AudioFrame &frame) {
-//	NDIlib_recv_free_audio_v3(instance_, &frame);
-//}
-//template<>
-//bool ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::captureFrame(ofxNDI::AudioFrame &frame) {
-//	return NDIlib_recv_capture_v3(instance_, nullptr, &frame, nullptr, timeout_ms_) == NDIlib_frame_type_audio;
-//}
-//bool ofxNDIRecvAudioFrameSync::captureFrame(ofxNDI::AudioFrame &frame) {
-//	NDIlib_framesync_capture_audio_v2(sync_, &frame, sample_rate_, num_channels_, num_samples_);
-//	return frame.no_channels != 0;
-//}
-//void ofxNDIRecvAudioFrameSync::freeFrame(ofxNDI::AudioFrame &frame) {
-//	NDIlib_framesync_free_audio_v2(sync_, &frame);
-//}
-//int ofxNDIRecvAudioFrameSync::getNumQueuedSamples() const {
-//	return NDIlib_framesync_audio_queue_depth(sync_);
-//}
-//
-//
-//#pragma mark Metadata Stream
-//
-//template<>
-//int ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getQueue() const
-//{
-//	NDIlib_recv_queue_t frames;
-//	NDIlib_recv_get_queue(instance_, &frames);
-//	return frames.metadata_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getNumReceivedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, &frames, nullptr);
-//	return frames.metadata_frames;
-//}
-//template<>
-//int64_t ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getNumDroppedFrame() const
-//{
-//	NDIlib_recv_performance_t frames;
-//	NDIlib_recv_get_performance(instance_, nullptr, &frames);
-//	return frames.metadata_frames;
-//}
-//template<>
-//bool ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::captureFrame(ofxNDI::MetadataFrame &frame) {
-//	return NDIlib_recv_capture_v2(instance_, nullptr, nullptr, &frame, timeout_ms_) == NDIlib_frame_type_metadata;
-//}
-//template<>
-//void ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::freeFrame(ofxNDI::MetadataFrame &frame) {
-//	NDIlib_recv_free_metadata(instance_, &frame);
-//}
-//
-//#pragma mark AVSync
-//
-//ofxNDIRecvAVSyncAudio::~AVSyncAudio()
-//{
-//	unsubscribe_();
-//	if(av_sync_) {
-//		NDIlib_avsync_destroy(av_sync_);
-//	}
-//}
-//void ofxNDIRecvAVSyncAudio::setup(ofxNDIReceiver &receiver)
-//{
-//	Threading::setup(receiver);
-//	av_sync_ = NDIlib_avsync_create(receiver.getInstance());
-//}
-//void ofxNDIRecvAVSyncAudio::setupSourceStream(Stream<SourceFrame> &source)
-//{
-//	unsubscribe_();
-//	ofAddListener(source.onFrameReceived, this, &ofxNDIRecvAVSyncAudio::onSourceFrameReceived);
-//	unsubscribe_ = [&]() {
-//		ofRemoveListener(source.onFrameReceived, this, &ofxNDIRecvAVSyncAudio::onSourceFrameReceived);
-//	};
-//}
-//void ofxNDIRecvAVSyncAudio::onSourceFrameReceived(const SourceFrame &source)
-//{
-//	std::lock_guard<std::mutex> lock(source_mtx_);
-//	source_frame_ = &source;
-//}
-//
-//bool ofxNDIRecvAVSyncAudio::captureFrame(ofxNDI::AudioFrame &frame)
-//{
-//	std::lock_guard<std::mutex> lock(source_mtx_);
-//	return NDIlib_avsync_synchronize(av_sync_, source_frame_, &frame_.back()) >= NDIlib_avsync_ret_success;
-//}
-//void ofxNDIRecvAVSyncAudio::freeFrame(ofxNDI::AudioFrame &frame)
-//{
-//	NDIlib_avsync_free_audio(av_sync_, &frame);
-//}
+
+template<>
+void ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::freeFrame(ofxNDI::AudioFrame &frame) {
+	NDIlib_recv_free_audio_v2(instance_, &frame);
+}
+template<>
+bool ofxNDI::Recv::Stream<ofxNDI::AudioFrame>::captureFrame(ofxNDI::AudioFrame &frame) {
+	return NDIlib_recv_capture_v2(instance_, nullptr, &frame, nullptr, timeout_ms_) == NDIlib_frame_type_audio;
+}
+bool ofxNDIRecvAudioFrameSync::captureFrame(ofxNDI::AudioFrame &frame) {
+	NDIlib_framesync_capture_audio(sync_, &frame, sample_rate_, num_channels_, num_samples_);
+	return frame.no_channels != 0;
+}
+void ofxNDIRecvAudioFrameSync::freeFrame(ofxNDI::AudioFrame &frame) {
+	NDIlib_framesync_free_audio(sync_, &frame);
+}
+int ofxNDIRecvAudioFrameSync::getNumQueuedSamples() const {
+	return NDIlib_framesync_audio_queue_depth(sync_);
+}
+
+
+#pragma mark Metadata Stream
+
+template<>
+int ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getQueue() const
+{
+	NDIlib_recv_queue_t frames;
+	NDIlib_recv_get_queue(instance_, &frames);
+	return frames.metadata_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getNumReceivedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, &frames, nullptr);
+	return frames.metadata_frames;
+}
+template<>
+int64_t ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::getNumDroppedFrame() const
+{
+	NDIlib_recv_performance_t frames;
+	NDIlib_recv_get_performance(instance_, nullptr, &frames);
+	return frames.metadata_frames;
+}
+template<>
+bool ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::captureFrame(ofxNDI::MetadataFrame &frame) {
+	return NDIlib_recv_capture_v2(instance_, nullptr, nullptr, &frame, timeout_ms_) == NDIlib_frame_type_metadata;
+}
+template<>
+void ofxNDI::Recv::Stream<ofxNDI::MetadataFrame>::freeFrame(ofxNDI::MetadataFrame &frame) {
+	NDIlib_recv_free_metadata(instance_, &frame);
+}
+
+#pragma mark AVSync
+
+ofxNDIRecvAVSyncAudio::~AVSyncAudio()
+{
+	unsubscribe_();
+	if(av_sync_) {
+		NDIlib_framesync_destroy(av_sync_);
+	}
+}
+void ofxNDIRecvAVSyncAudio::setup(ofxNDIReceiver &receiver)
+{
+	Threading::setup(receiver);
+	av_sync_ = NDIlib_framesync_create(receiver.getInstance());
+}
+void ofxNDIRecvAVSyncAudio::setupSourceStream(Stream<SourceFrame> &source)
+{
+	unsubscribe_();
+	ofAddListener(source.onFrameReceived, this, &ofxNDIRecvAVSyncAudio::onSourceFrameReceived);
+	unsubscribe_ = [&]() {
+		ofRemoveListener(source.onFrameReceived, this, &ofxNDIRecvAVSyncAudio::onSourceFrameReceived);
+	};
+}
+void ofxNDIRecvAVSyncAudio::onSourceFrameReceived(const SourceFrame &source)
+{
+	std::lock_guard<std::mutex> lock(source_mtx_);
+	source_frame_ = &source;
+}
+
+bool ofxNDIRecvAVSyncAudio::captureFrame(ofxNDI::AudioFrame &frame)
+{
+	std::lock_guard<std::mutex> lock(source_mtx_);
+	return false; // NDIlib_framesync_synchronize(av_sync_, source_frame_, &frame_.back()) >= NDIlib_framesync_ret_success;
+}
+void ofxNDIRecvAVSyncAudio::freeFrame(ofxNDI::AudioFrame &frame)
+{
+	NDIlib_framesync_free_audio(av_sync_, &frame);
+}
--- src/ofxNDIRecvStream.h
+++ src/ofxNDIRecvStream.h
@@ -1,187 +1,187 @@
-//#pragma once
-//
-//#include <stddef.h>
-//#include <Processing.NDI.Lib.h>
-//#include "ofxNDIFrame.h"
-//#include "ofxNDIReceiver.h"
-//#include "ofxNDI.h"
-//#include "ofThread.h"
-//#include "DoubleBuffer.h"
-//#include "ofEvents.h"
-//
-//namespace ofxNDI {
-//namespace Recv {
-//
-//template<typename FrameType, typename Wrapper=ofxNDIReceiver>
-//class Stream
-//{
-//public:
-//	virtual void setup(Wrapper &wrapper) { instance_ = wrapper.getInstance(); }
-//	virtual void update(){}
-//	bool isFrameNew() const { return is_frame_new_; }
-//	virtual const FrameType& getFrame() const=0;
-//	template<typename Output> void decodeTo(Output &dst) const { Frame::decode(getFrame(), dst); }
-//	void setTimeout(uint32_t milliseconds) { timeout_ms_ = milliseconds; }
-//
-//	std::string getMetadata() const { 
-//		static_assert(!std::is_same<FrameType, ofxNDI::MetadataFrame>::value, "this function is not for ofxNDIRecvMetadata");
-//		auto ptr = getFrame().p_metadata;
-//		return ptr ? ptr : "";
-//	}
-//
-//	int getQueue() const;
-//	int64_t getNumReceivedFrame() const;
-//	int64_t getNumDroppedFrame() const;
-//
-//	ofEvent<const FrameType> onFrameReceived;
-//
-//	template<typename Allocator, typename Deallocator>
-//	void setAllocator(void* p_opaque, Allocator p_allocator, Deallocator p_deallocator);
-//
-//protected:
-//	typename Wrapper::Instance instance_;
-//	bool is_frame_new_=false;
-//	uint32_t timeout_ms_=1000;
-//
-//	virtual bool captureFrame(FrameType &frame);
-//	virtual void freeFrame(FrameType &frame);
-//};
-//
-//template<typename FrameType, typename Wrapper=ofxNDIReceiver>
-//class Blocking : public Stream<FrameType, Wrapper>
-//{
-//public:
-//	void update() override {
-//		FrameType frame;
-//		if((is_frame_new_ = captureFrame(frame))) {
-//			std::swap(frame, frame_);
-//			ofNotifyEvent(Stream<FrameType, Wrapper>::onFrameReceived, getFrame(), this);
-//			freeFrame(frame);
-//		}
-//	}
-//	const FrameType& getFrame() const override { return frame_; }
-//	bool isFrameNew() const { return is_frame_new_; }
-//protected:
-//	using Stream<FrameType, Wrapper>::captureFrame;
-//	using Stream<FrameType, Wrapper>::freeFrame;
-//	FrameType frame_;
-//	bool is_frame_new_=false;
-//};
-//template<typename FrameType, typename Wrapper=ofxNDIReceiver>
-//class Threading : public Stream<FrameType, Wrapper>, private ofThread
-//{
-//public:
-//	void setup(Wrapper &wrapper) override {
-//		Stream<FrameType, Wrapper>::setup(wrapper);
-//		startThread();
-//	}
-//	~Threading() {
-//		if(isThreadRunning()) {
-//			waitForThread();
-//		}
-//	}
-//	void update() override {
-//		std::lock_guard<std::mutex> lock(mutex_);
-//		is_frame_new_ = has_new_frame_;
-//		has_new_frame_ = false;
-//	}
-//	
-//	void threadedFunction() override {
-//		while(isThreadRunning()) {
-//			updateFrame();
-//			std::this_thread::yield();
-//		}
-//	}
-//	void updateFrame() {
-//		if(captureFrame(frame_.back())) {
-//			std::lock_guard<std::mutex> lock(mutex_);
-//			frame_.swap();
-//			has_new_frame_ = true;
-//			ofNotifyEvent(Stream<FrameType, Wrapper>::onFrameReceived, getFrame(), this);
-//			freeFrame(frame_.back());
-//		}
-//	}
-//	const FrameType& getFrame() const override { return frame_.front(); }
-//	template<typename Output> void decodeTo(Output &dst) const {
-//		std::lock_guard<std::mutex> lock(mutex_);
-//		Stream<FrameType, Wrapper>::decodeTo(dst);
-//	}
-//
-//	bool isFrameNew() const { return is_frame_new_; }
-//protected:
-//	mutable std::mutex mutex_;
-//	DoubleBuffer<FrameType> frame_;
-//	bool is_frame_new_=false;
-//	bool has_new_frame_=false;
-//
-//	using Stream<FrameType, Wrapper>::captureFrame;
-//	using Stream<FrameType, Wrapper>::freeFrame;
-//};
-//template<typename FrameType>
-//class FrameSync : public Blocking<FrameType, ofxNDIReceiver>
-//{
-//public:
-//	void setup(ofxNDIReceiver &wrapper) override {
-//		Blocking<FrameType, ofxNDIReceiver>::setup(wrapper);
-//		sync_ = wrapper.getFrameSync();
-//		if(!sync_) {
-//			sync_ = wrapper.createFrameSync();
-//		}
-//	}
-//protected:
-//	NDIlib_framesync_instance_t sync_;
-//};
-//
-//class FrameSyncVideo : public FrameSync<ofxNDI::VideoFrame>
-//{
-//public:
-//	void setFieldType(NDIlib_frame_format_type_e format) { field_type_ = format; }
-//protected:
-//	bool captureFrame(ofxNDI::VideoFrame &frame) override;
-//	void freeFrame(ofxNDI::VideoFrame &frame) override;
-//	NDIlib_frame_format_type_e field_type_=NDIlib_frame_format_type_progressive;
-//};
-//
-//class FrameSyncAudio : public FrameSync<ofxNDI::AudioFrame>
-//{
-//public:
-//	void setSampleRate(int sample_rate) { sample_rate_ = sample_rate; }
-//	void setNumChannels(int num) { num_channels_ = num; }
-//	void setNumSamples(int num) { num_samples_ = num; }
-//	int getNumQueuedSamples() const;
-//protected:
-//	bool captureFrame(ofxNDI::AudioFrame &frame) override;
-//	void freeFrame(ofxNDI::AudioFrame &frame) override;
-//	int sample_rate_=0, num_channels_=0, num_samples_=0;
-//};
-//
-//class AVSyncAudio : public Threading<ofxNDI::AudioFrame>
-//{
-//public:
-//	~AVSyncAudio();
-//	using SourceFrame = ofxNDI::VideoFrame;
-//	
-//	void setup(ofxNDIReceiver &receiver) override;
-//	void setupSourceStream(Stream<SourceFrame> &source);
-//
-//protected:
-//	NDIlib_avsync_instance_t av_sync_;
-//	void onSourceFrameReceived(const SourceFrame &source);
-//	std::function<void()> unsubscribe_=[]{};
-//
-//	mutable std::mutex source_mtx_;
-//	const SourceFrame *source_frame_;
-//	bool captureFrame(ofxNDI::AudioFrame &frame) override;
-//	void freeFrame(ofxNDI::AudioFrame &frame) override;
-//};
-//
-//
-//}}
-//using ofxNDIRecvVideoBlocking = ofxNDI::Recv::Blocking<ofxNDI::VideoFrame>;
-//using ofxNDIRecvVideoThreading = ofxNDI::Recv::Threading<ofxNDI::VideoFrame>;
-//using ofxNDIRecvVideoFrameSync = ofxNDI::Recv::FrameSyncVideo;
-//using ofxNDIRecvAudioBlocking = ofxNDI::Recv::Blocking<ofxNDI::AudioFrame>;
-//using ofxNDIRecvAudioThreading = ofxNDI::Recv::Threading<ofxNDI::AudioFrame>;
-//using ofxNDIRecvAudioFrameSync = ofxNDI::Recv::FrameSyncAudio;
-//using ofxNDIRecvAVSyncAudio = ofxNDI::Recv::AVSyncAudio;
-//using ofxNDIRecvMetadata = ofxNDI::Recv::Blocking<ofxNDI::MetadataFrame>;
+#pragma once
+
+#include <stddef.h>
+#include <Processing.NDI.Lib.h>
+#include "ofxNDIFrame.h"
+#include "ofxNDIReceiver.h"
+#include "ofxNDI.h"
+#include "ofThread.h"
+#include "DoubleBuffer.h"
+#include "ofEvents.h"
+
+namespace ofxNDI {
+namespace Recv {
+
+template<typename FrameType, typename Wrapper=ofxNDIReceiver>
+class Stream
+{
+public:
+	virtual void setup(Wrapper &wrapper) { instance_ = wrapper.getInstance(); }
+	virtual void update(){}
+	bool isFrameNew() const { return is_frame_new_; }
+	virtual const FrameType& getFrame() const=0;
+	template<typename Output> void decodeTo(Output &dst) const { Frame::decode(getFrame(), dst); }
+	void setTimeout(uint32_t milliseconds) { timeout_ms_ = milliseconds; }
+
+	std::string getMetadata() const { 
+		static_assert(!std::is_same<FrameType, ofxNDI::MetadataFrame>::value, "this function is not for ofxNDIRecvMetadata");
+		auto ptr = getFrame().p_metadata;
+		return ptr ? ptr : "";
+	}
+
+	int getQueue() const;
+	int64_t getNumReceivedFrame() const;
+	int64_t getNumDroppedFrame() const;
+
+	ofEvent<const FrameType> onFrameReceived;
+
+	//template<typename Allocator, typename Deallocator>
+	//void setAllocator(void* p_opaque, Allocator p_allocator, Deallocator p_deallocator);
+
+protected:
+	typename Wrapper::Instance instance_;
+	bool is_frame_new_=false;
+	uint32_t timeout_ms_=1000;
+
+	virtual bool captureFrame(FrameType &frame);
+	virtual void freeFrame(FrameType &frame);
+};
+
+template<typename FrameType, typename Wrapper=ofxNDIReceiver>
+class Blocking : public Stream<FrameType, Wrapper>
+{
+public:
+	void update() override {
+		FrameType frame;
+		if((is_frame_new_ = captureFrame(frame))) {
+			std::swap(frame, frame_);
+			ofNotifyEvent(Stream<FrameType, Wrapper>::onFrameReceived, getFrame(), this);
+			freeFrame(frame);
+		}
+	}
+	const FrameType& getFrame() const override { return frame_; }
+	bool isFrameNew() const { return is_frame_new_; }
+protected:
+	using Stream<FrameType, Wrapper>::captureFrame;
+	using Stream<FrameType, Wrapper>::freeFrame;
+	FrameType frame_;
+	bool is_frame_new_=false;
+};
+template<typename FrameType, typename Wrapper=ofxNDIReceiver>
+class Threading : public Stream<FrameType, Wrapper>, private ofThread
+{
+public:
+	void setup(Wrapper &wrapper) override {
+		Stream<FrameType, Wrapper>::setup(wrapper);
+		startThread();
+	}
+	~Threading() {
+		if(isThreadRunning()) {
+			waitForThread();
+		}
+	}
+	void update() override {
+		std::lock_guard<std::mutex> lock(mutex_);
+		is_frame_new_ = has_new_frame_;
+		has_new_frame_ = false;
+	}
+	
+	void threadedFunction() override {
+		while(isThreadRunning()) {
+			updateFrame();
+			std::this_thread::yield();
+		}
+	}
+	void updateFrame() {
+		if(captureFrame(frame_.back())) {
+			std::lock_guard<std::mutex> lock(mutex_);
+			frame_.swap();
+			has_new_frame_ = true;
+			ofNotifyEvent(Stream<FrameType, Wrapper>::onFrameReceived, getFrame(), this);
+			freeFrame(frame_.back());
+		}
+	}
+	const FrameType& getFrame() const override { return frame_.front(); }
+	template<typename Output> void decodeTo(Output &dst) const {
+		std::lock_guard<std::mutex> lock(mutex_);
+		Stream<FrameType, Wrapper>::decodeTo(dst);
+	}
+
+	bool isFrameNew() const { return is_frame_new_; }
+protected:
+	mutable std::mutex mutex_;
+	DoubleBuffer<FrameType> frame_;
+	bool is_frame_new_=false;
+	bool has_new_frame_=false;
+
+	using Stream<FrameType, Wrapper>::captureFrame;
+	using Stream<FrameType, Wrapper>::freeFrame;
+};
+template<typename FrameType>
+class FrameSync : public Blocking<FrameType, ofxNDIReceiver>
+{
+public:
+	void setup(ofxNDIReceiver &wrapper) override {
+		Blocking<FrameType, ofxNDIReceiver>::setup(wrapper);
+		sync_ = wrapper.getFrameSync();
+		if(!sync_) {
+			sync_ = wrapper.createFrameSync();
+		}
+	}
+protected:
+	NDIlib_framesync_instance_t sync_;
+};
+
+class FrameSyncVideo : public FrameSync<ofxNDI::VideoFrame>
+{
+public:
+	void setFieldType(NDIlib_frame_format_type_e format) { field_type_ = format; }
+protected:
+	bool captureFrame(ofxNDI::VideoFrame &frame) override;
+	void freeFrame(ofxNDI::VideoFrame &frame) override;
+	NDIlib_frame_format_type_e field_type_=NDIlib_frame_format_type_progressive;
+};
+
+class FrameSyncAudio : public FrameSync<ofxNDI::AudioFrame>
+{
+public:
+	void setSampleRate(int sample_rate) { sample_rate_ = sample_rate; }
+	void setNumChannels(int num) { num_channels_ = num; }
+	void setNumSamples(int num) { num_samples_ = num; }
+	int getNumQueuedSamples() const;
+protected:
+	bool captureFrame(ofxNDI::AudioFrame &frame) override;
+	void freeFrame(ofxNDI::AudioFrame &frame) override;
+	int sample_rate_=0, num_channels_=0, num_samples_=0;
+};
+
+class AVSyncAudio : public Threading<ofxNDI::AudioFrame>
+{
+public:
+	~AVSyncAudio();
+	using SourceFrame = ofxNDI::VideoFrame;
+	
+	void setup(ofxNDIReceiver &receiver) override;
+	void setupSourceStream(Stream<SourceFrame> &source);
+
+protected:
+	NDIlib_framesync_instance_t av_sync_;
+	void onSourceFrameReceived(const SourceFrame &source);
+	std::function<void()> unsubscribe_=[]{};
+
+	mutable std::mutex source_mtx_;
+	const SourceFrame *source_frame_;
+	bool captureFrame(ofxNDI::AudioFrame &frame) override;
+	void freeFrame(ofxNDI::AudioFrame &frame) override;
+};
+
+
+}}
+using ofxNDIRecvVideoBlocking = ofxNDI::Recv::Blocking<ofxNDI::VideoFrame>;
+using ofxNDIRecvVideoThreading = ofxNDI::Recv::Threading<ofxNDI::VideoFrame>;
+using ofxNDIRecvVideoFrameSync = ofxNDI::Recv::FrameSyncVideo;
+using ofxNDIRecvAudioBlocking = ofxNDI::Recv::Blocking<ofxNDI::AudioFrame>;
+using ofxNDIRecvAudioThreading = ofxNDI::Recv::Threading<ofxNDI::AudioFrame>;
+using ofxNDIRecvAudioFrameSync = ofxNDI::Recv::FrameSyncAudio;
+using ofxNDIRecvAVSyncAudio = ofxNDI::Recv::AVSyncAudio;
+using ofxNDIRecvMetadata = ofxNDI::Recv::Blocking<ofxNDI::MetadataFrame>;
